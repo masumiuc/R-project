6fa5b7bbd442263452173e8284dc34575f33a263
Abdul Kader <masumiuc@users.noreply.github.com>
2023-07-22 18:17:43 +0600

    vector and pipe added with examples

--- rlearn.R
+++ rlearn.R
@@ -1,3 +1,8 @@
 print("hello world")
 #Here is an example of a variable
 first_veriable <-"This is my variable"
+second_variable <- 12.5
+#Example of Vector: a vector is a group of data elements of the same type stored in a sequence in R.
+vec_1 <- c(13, 48.5, 71, 101.5, 2)
+vec_1
+# A Pipe is  a tool in R for expressing a sequence of multiple operations. A pipe is represented by a % sign, followed by a > sign, and another % sign (%>%). 
